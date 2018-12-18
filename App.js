2df7e555e26c49bd5dbc8ed2657b95f746f628ec
Tharin <tharin2501@hotmail.no>
2018-12-18 15:08:31 +0100

    // created profil page

--- App.js
+++ App.js
@@ -1,21 +1,96 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
+
+import AnimatedBar from "react-native-animated-bar";
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hans Ludvig er best</Text>
-      </View>
-    );
-  }
+    state = {
+        progress: 0,
+    };
+
+    componentDidMount() {
+        const interval = setInterval(() => {
+            if (this.state.progress > 0.9) return clearInterval(interval);
+            this.setState(state => {
+                return {
+                    progress: state.progress + 0.1, // incrementer med antall xp for faen
+                };
+            });
+        }, 1000);
+    }
+
+    render() {
+        return (
+            <View>
+                <Text style={textStyle.overText}>-Min Profil-</Text>
+                <Text style={textStyle.underText}>-Navn-</Text>
+
+                <View style={imgStyle.imgPos}>
+                    <Image style={imgStyle.image}
+                           source={require('./assets/avatar.png')}/>
+                </View>
+
+                <View style={barStyle.bar}>
+                    <AnimatedBar
+                        progress={this.state.progress}
+                        height={20}
+                        borderColor="grey"
+                        fillColor="black"
+                        barColor="orange"
+                        borderRadius={5}
+                        borderWidth={5}
+                        duration={500}
+                    >
+                    </AnimatedBar>
+                </View>
+
+                <View>
+                    <Text style={textStyle.underText}>Min Achievements</Text>
+                </View>
+
+            </View>
+
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#00eeff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+const textStyle = StyleSheet.create({
+    overText: {
+        color: 'black',
+        fontWeight: 'bold',
+        fontSize: 35,
+        textAlignVertical: 'top',
+        textAlign: 'center',
+        padding: 20,
+    },
+    underText: {
+        color: 'black',
+        fontSize: 25,
+        textAlign: 'center',
+        justifyContent: 'space-between',
+        padding: 10,
+    },
+});
+
+const imgStyle = StyleSheet.create({
+    image: {
+        height: 150,
+        borderRadius: 75,
+        width: 150,
+    },
+    imgPos: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+    },
+});
+
+const barStyle = StyleSheet.create({
+    bar: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
 });
